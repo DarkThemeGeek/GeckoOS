0ce87f3649f8bc142fa61049d1a71c62c841ff24
DarkThemeGeek <imperator09@atomicmail.io>
2026-08-15 22:06:30 +0300

    added docs to the virtual memory manager for future reference

--- include/mem/paging.h
+++ include/mem/paging.h
@@ -1,4 +1,5 @@
 #pragma once
+//VMM-Virtual memory manager
 #include "drivers/tables/isr.h"
 #include <stdint.h>
 #include <stdbool.h>
@@ -34,15 +35,24 @@ typedef struct {
 static inline uint64_t pte_get_phys(pte_t e)   { return e & PTE_ADDR_MASK; }
 static inline bool     pte_is_present(pte_t e) { return (e & PTE_PRESENT) != 0; }
 
+//Initializing the memory
 bool          vmm_init(void);
+//maping an adress from it s physical position to a virtual one for the page table
 bool          vmm_map(page_table_t *pml4, uint64_t phys, uint64_t virt, uint64_t flags);
+//unmaping an adress from it s virtual one to the given page table
 void          vmm_unmap(page_table_t *pml4, uint64_t virt);
+//get the real adress from a virtual one in a page table
 uint64_t      vmm_get_phys(page_table_t *pml4, uint64_t virt);
+//get the current page table
 page_table_t *vmm_get_pml4(void);
+//set the current page table
 bool          vmm_set_pml4(page_table_t *pml4);
-
+//map a memory region to the current page table from the given adress and size of the region
 uint64_t      mmio_map(uint64_t phys, uint64_t size);
+//unmapping a region from the current page table
 int mmio_unmap(uint64_t virt, uint64_t size);
+//page fault handler function called by the isr(interupt) which handles the page faults 
 void page_fault(registers_t* regs);
+//allocs a page table
 page_table_t *alloc_table(void);
 // uint64_t mmio_map1(uint64_t phys, uint64_t virt, uint64_t flags, uint64_t size);
